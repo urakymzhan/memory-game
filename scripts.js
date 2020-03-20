93a8dce6149de874160d3d55c20b65f8f9cefaa1
Ulan Rakymzhanov <ulan@carta.healthcare>
2020-03-20 16:56:44 -0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// 1. get all memory cards
+
+// 2. add event listener to each card and and flip when clicked
+
+// 3. Define isFlippedCard=false, lockboard=false, firstCard and secondCard variables
+
+// 4. Create a flipCard function
+
+// 5. Create a checkForMatch function ( inside check if dataset from HTML element of both firstCard and secondCard are same)
+
+// 6. If same call disableCards function
+
+// 7 if not same call unflipCards function
+
+// 8. In each disbaleCards function unflipCards function reset all cards variables and flags
+
+// 9. Shuffle html element orders on page reload
+
+// 10. Make sure 3rd card not clicked before 2 are being unflipped.
+
